de7e195aecf8dab3b1311eeba2ee8e072ca49618
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-01-11 17:39:51 +0900

    add : add user post root

--- app/routes/v1/user.js
+++ app/routes/v1/user.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const UserModel = require('../../models/userModel.js');
 
-// GET  http://localhost:3000/api/v1/user/test
-router.get('/test', function(req, res) {
-  res.json({
-    message: 'This is user api'
+router.post('/', function(req, res) {
+  // モデル作成．
+  const User = new UserModel();
+
+  // データを詰め込む
+  User.name = req.body.name;
+  User.screen_name = req.body.screen_name;
+  User.bio = req.body.bio;
+
+  // 保存処理
+  User.save(function(err) {
+    if (err) {
+      // エラーがあった場合エラーメッセージを返す
+      res.send(err);
+    } else {
+      // エラーがなければ「Success!!」
+      res.json({ message: 'Success!!' });
+    }
   });
 });
 
