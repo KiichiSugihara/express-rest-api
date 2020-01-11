bfa6300f7c7b25dc5669133a42896140b2468f99
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2020-01-11 16:21:59 +0900

    add : add v1 user

--- app/routes/v1/index.js
+++ app/routes/v1/index.js
@@ -1,14 +1,13 @@
 const express = require('express');
-// ルーティングするで
+// ルーティング
 const router = express.Router();
 router.use('/article', require('./article.js'));
+router.use('/user', require('./user.js'));
 
-// routerにルーティングの動作を書いてく
 router.get('/', function(req, res) {
   res.json({
     message: 'Hello,world'
   });
 });
 
-//routerをモジュールとして扱う準備
 module.exports = router;
